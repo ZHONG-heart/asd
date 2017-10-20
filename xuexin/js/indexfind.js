a39bf3d34198f79209da9c03252984a33886b40a
ZHONG-heart <953665604@qq.com>
2017-10-20 22:15:50 +0800

    Add files via upload

---
+++ xuexin/js/indexfind.js
@@ -0,0 +1,9 @@
+$('#headerList').mouseover(function(){
+	
+	$('#show').css('display','block')
+	$('.List2').css('display','block')
+})
+$('#show').mouseout(function(){
+	$('#show').css('display','none')
+	$('.List2').css('display','none')
+})
