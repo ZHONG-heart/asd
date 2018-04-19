9628e687d2b767f6276274a99c9f4f7d586de729
ZHONG-heart <953665604@qq.com>
2018-04-19 18:31:16 +0800

    Delete indexfind.js

--- xuexin/js/indexfind.js
+++
@@ -1,9 +0,0 @@
-$('#headerList').mouseover(function(){
-	
-	$('#show').css('display','block')
-	$('.List2').css('display','block')
-})
-$('#show').mouseout(function(){
-	$('#show').css('display','none')
-	$('.List2').css('display','none')
-})
